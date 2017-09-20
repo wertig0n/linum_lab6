6e17666c801e917066f6662d7253d16063711723
carwa <calle.wallin@gmail.com>
2017-09-20 17:31:39 +0200

    Flyttade filer

---
+++ src/power.c
@@ -0,0 +1,15 @@
+//Funktion för att räkna ut effekt
+#include "power.h"
+
+float calc_power_r(float volt, float resistance){
+
+
+return((volt*volt)/resistance);
+
+}
+
+float calc_power_i(float volt, float current){
+
+return(volt*current);
+}
+
