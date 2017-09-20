015fda0fc1d65e124372552b8c7903de78881224
Alvine <alvine@tamkin.se>
2017-09-20 12:09:20 +0200

    Added includes to libraries, and function calc_power_r

--- src/electrotest.c
+++ src/electrotest.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "libresistance.h"
+#include "libpower.h"
+#include "libcomponent.h"
 
 int main() {
 	int count;
 	int c;
 	float array[100];
 	char conn;
+	int volt;
+
+	printf("Ange spänningskälla i V: ");
+	scanf("%d", &volt);
 
 	printf("Ange koppling: ");
 	scanf(" %c", &conn);
@@ -19,9 +25,17 @@ int main() {
 		scanf("%f", &array[c]);
 	}
  
-	float total = calc_resistance(count, conn, array);	
- 
-	printf("Ersättningsresistans: %0.1f\n", total);
+	// Beräkna resistansen
+	float resistance = calc_resistance(count, conn, array);	
+	printf("Ersättningsresistans:\n%0.1f ohm\n", resistance);
+
+	// Beräkna effekten
+	float P = calc_power_r(volt, resistance);
+	printf("Effekt:\n%0.2f W\n", P);
+
+	// Beräkna ersättningsresistanser
+	// int count = e_resistance(resistance, res_array);
+	// printf("Ersättningsresistanser i E12-serien kopplade i serie:\n");
 
 	return 0;
 }
