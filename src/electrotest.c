fca3f5b7009848f8bb431e6b982942e2ff53fa26
Per Ekström <per.ekstrom@alleato.se>
2017-09-23 21:27:47 +0200

    Fixed up electrotest

--- src/electrotest.c
+++ src/electrotest.c
@@ -1,50 +1,60 @@
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
 #include "libresistance.h"
 #include "libpower.h"
 #include "libcomponent.h"
 
 int main() {
-	int count;
-	int c;
-	float array[100];
-	float res[] = { 0.0, 0.0, 0.0 };
+	char input[10] = {0};  /* input buffer */
+	char *res =0;          /* Make sure fgets work properly... */
+	float *values = 0;     /* Contains all values we put in */
 	char conn;
+	float resistance=0.0, power=0.0;
 	int volt;
+	int res_count=0, e_count=0;
+	int c;
 
 	printf("Ange spänningskälla i V: ");
-	scanf("%d", &volt);
+	res = fgets(input, 10, stdin);
+	volt = atoi(input);
 
 	printf("Ange koppling: ");
-	scanf(" %c", &conn);
+	res = fgets(input, 10, stdin);
+	conn = input[0];
 
 	printf("Antal komponenter: ");
-	scanf("%d", &count);
+	res = fgets(input, 10, stdin);	
+	res_count = atoi(input);
 
-	for(c = 0; c < count; c++) {
+	values = calloc(res_count+3,sizeof(float)); /* Dynamicly allocate array */
+
+	for(c = 0; c < res_count; c++) {
 		printf("Komponent %d i ohm: ", c + 1);
-		scanf("%f", &array[c]);
+		res = fgets(input, 10, stdin);
+		values[c] = atof(input);
 	}
  
 	// Beräkna resistansen
-	float resistance = calc_resistance(count, conn, array);	
+	resistance = calc_resistance(res_count, conn, values);	
 	printf("Ersättningsresistans:\n%0.1f ohm\n", resistance);
 
 	// Beräkna effekten
-	float P = calc_power_r(volt, resistance);
-	printf("Effekt:\n%0.2f W\n", P);
+	power = calc_power_r(volt, resistance);
+	printf("Effekt:\n%0.2f W\n", power);
 
 	// Beräkna ersättningsresistanser
-	count = e_resistance(resistance, res);
+	e_count = e_resistance(resistance, &values[res_count]);
 	printf("Ersättningsresistanser i E12-serien kopplade i serie:\n");
 
-	if (count == 0) {
+	if (e_count == 0) {
 		printf("Det finns inga resistanser som matchar!\n");
         }
 
-	for (c=0; c < count; c++) {
-		printf("%f\n", res[c]);
+	for (c=0; c < e_count; c++) {
+		printf("%f\n", values[res_count+c]);
 	}
+
+	free(values);
 
 	return 0;
 }
--- src/power.h
+++
@@ -1,9 +0,0 @@
-#ifndef _POWER_
-#define _POWER_
-#include <math.h>
-
-float calc_power_r(float volt, float resistance);
-
-float calc_power_i(float volt, float current);
-
-#endif
